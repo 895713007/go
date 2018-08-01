51172bcff06b511e8daf4b9b24a8bfc30c002acd
张宏文 <zhanghongwen@mytoken.io>
2018-08-01 15:25:49 +0800

    init 2

--- log/log.go
+++ log/log.go
@@ -2,95 +2,90 @@ package log
 
 import (
 	"fmt"
-	"os"
 	"time"
 )
+
+var (
+	c  *Config
+	hs = make([]handle, 0)
+)
+
+type handle interface {
+	info(format string, args ...interface{})
+	warn(format string, args ...interface{})
+	debug(format string, args ...interface{})
+	error(format string, args ...interface{})
+}
+
+type xtime time.Time
 
 // Config log config.
 type Config struct {
 	Stdout bool
-	Host   string
 	Dir    string
 	Agent  *AgentConfig
-	// VLevel Enable V-leveled logging at the specified _level.
-	VLevel int32
-	// Module=""
-	// The syntax of the argument is a map of pattern=N,
-	// where pattern is a literal file name (minus the ".go" suffix) or
-	// "glob" pattern and N is a V _level. For instance:
-	// [module]
-	//   "service" = 1
-	//   "dao*" = 2
-	// sets the V _level to 2 in all Go files whose names begin "dao".
-	Module map[string]int32
 }
 
-// D represents a map of entry _level data used for structured logging.
-type D map[string]interface{}
+// AgentConfig agent config.
+type AgentConfig struct {
+	TaskID  string
+	Proto   string
+	Addr    string
+	Buffer  int
+	Chan    int
+	Timeout time.Duration
+}
 
-var (
-	h Handler
-	c *Config
-)
+type D struct {
+	Index    string   `json:"@index"`
+	Type     string   `json:"@type"`
+	Time     xtime    `json:"datetime"`
+	UniqueID string   `json:"unique_id"`
+	UID      int      `json:"uid"`
+	Info     *logInfo `json:"info"`
+}
+
+type logInfo struct {
+	Host    string `json:"host"`
+	Extra   string `json:"extra"`
+	Message string `json:"message"`
+	Context string `json:"context"`
+}
+
+func (xt xtime) MarshalJSON() ([]byte, error) {
+	var stamp = fmt.Sprintf("\"%s\"", time.Time(xt).Format("2006-01-02 15:04:05"))
+	return []byte(stamp), nil
+}
 
 // Init create logger with context.
 func Init(conf *Config) {
-	var (
-		hs Handlers
-	)
-	conf.Host = os.Getenv("CONF_HOSTNAME")
-	if len(conf.Host) == 0 {
-		hn, _ := os.Hostname()
-		conf.Host = hn
+	if conf.Stdout == true {
+		hs = append(hs, stdInit())
 	}
-	c = conf
-	if conf.Stdout {
-		hs = append(hs, NewStdout())
+	if isDir(conf.Dir) {
+		hs = append(hs, fileInit(conf))
 	}
-	if conf.Dir != "" {
-		hs = append(hs, NewFile(conf.Dir))
-	}
-	if conf.Agent != nil {
-		hs = append(hs, NewAgent(conf.Agent))
-	}
-	h = hs
 }
 
-// Info logs a message at the info log _level.
 func Info(format string, args ...interface{}) {
-	logf(infoLevel, format, args...)
+	for _, h := range hs {
+		h.info(format, args...)
+	}
 }
 
-// Warn logs a message at the warning log _level.
 func Warn(format string, args ...interface{}) {
-	logf(warnLevel, format, args...)
+	for _, h := range hs {
+		h.warn(format, args...)
+	}
+}
+func Debug(format string, args ...interface{}) {
+	for _, h := range hs {
+		h.debug(format, args...)
+	}
 }
 
-// Error logs a message at the error log _level.
 func Error(format string, args ...interface{}) {
-	logf(errorLevel, format, args...)
-}
-
-func logf(lv Level, format string, args ...interface{}) {
-	if h == nil {
-		return
+	for _, h := range hs {
+		h.error(format, args...)
 	}
-	now := time.Now()
-	f := D{}
-	f[_appID] = "app_ID"
-	f[_instanceID] = c.Host
-	f[_levelValue] = lv
-	f[_level] = lv.String()
-	f[_time] = now.Format(_timeFormat)
-	f[_source] = funcName()
-	f[_log] = fmt.Sprintf(format, args...)
-	h.Log(lv, f)
-}
-
-// Close close resource.
-func Close() (err error) {
-	if h == nil {
-		return
-	}
-	return h.Close()
 }
---
+++ log/std.go
@@ -0,0 +1,27 @@
+package log
+
+import "github.com/Sirupsen/logrus"
+
+type stdHandler struct {
+	logger *logrus.Logger
+}
+
+func stdInit() *stdHandler {
+	return new(stdHandler)
+}
+
+func (fh *stdHandler) info(format string, args ...interface{}) {
+	fh.logger.Infof(format, args...)
+}
+
+func (fh *stdHandler) warn(format string, args ...interface{}) {
+	fh.logger.Warnf(format, args...)
+}
+
+func (fh *stdHandler) debug(format string, args ...interface{}) {
+	fh.logger.Debugf(format, args...)
+}
+
+func (fh *stdHandler) error(format string, args ...interface{}) {
+	fh.logger.Errorf(format, args...)
+}
