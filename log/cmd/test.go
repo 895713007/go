3ffab9bbfc9d1b6b9549c8d4a8e2e015b6b192f8
henter <henter@henter.me>
2018-08-22 18:18:10 +0800

    log

---
+++ log/cmd/test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"mytoken/go_sdk/log"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.WithField("kk", "vv", "xxx").Infof("test debug log %s", "ddd")
+	log.Infof("test info log %s", "ddd")
+	log.Warnf("test warn log %s", "ddd")
+
+	log.RefreshUniqueId()
+
+	log.Errorf("test error log %s", "ddd")
+	log.WithFields(logrus.Fields{"aa":"bb", "cc": logrus.Fields{"dd":"aaaaaaa"}}, "test").Infof("test obj")
+
+	log.Type("test").Info("fdsafdas")
+}
